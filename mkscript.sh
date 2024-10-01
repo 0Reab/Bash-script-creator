dde13cc3b13069579a7fdf9496f4e803d8344cb5
0Reab <122540000+0Reab@users.noreply.github.com>
2024-10-01 15:03:25 +0200

    Added python scripts functionality
    ./mkscript script_name py # makes python script w/ path python3

--- mkscript.sh
+++ mkscript.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 # One tip: Putting this script in /usr/local/bin and removing .sh extension will make this easy to run globally
 
-green='\033[1;92m'     # txt color
+green='\033[1;92m'                              # txt color
 red='\033[1;91m'
-name="$1"
-bash_path="#!$(which bash)"    # shebang bash path
-
+name="$1"                                                       # script name
+bash_path="#!$(which bash)"             # shebang path bash
+python_path="#!$(which python3)"        # shebang path python3
+is_py_script=false                              # defualting to bash
 
 function usage() {
-	printf "\n${green}Usage: $(basename "$0") <script_name>" && exit 1
+        printf "\n${green}Usage: $(basename "$0") <script_name>" && exit 1
 }
 
 
 function error() {
-	printf "\n${red}Error occurred..." && exit 1
+        printf "\n${red}Error occurred..." && exit 1
+}
+
+
+function add_ext() {
+        local ext="$1"
+        [[ ! "$name" = *."$ext" ]] && name="${name}.$ext"   # add extension if needed
+}
+
+
+function create_script() {
+        local path="$1"
+        touch "$name" && chmod +x "$name" && echo "$path" >> "$name" && printf "${green}Created $name" || error
 }
 
 
 function main() {
-	[ $# -ne 1 ] && usage    # check num of args
 
-	[[ ! "$name" = *.sh ]] && name="${name}.sh"   # add .sh extension if needed
+        [[ $# -ge 1 && $# -le 2 ]] || usage    # check num of args
 
-	[ -e "$name" ] && printf "\n${red}$name already exists" && error    # check if file with the same name exits
+        [ "$2" == "py" ] && is_py_script=true
 
-	# create file with perms and bash path
-	
-	touch "$name" && chmod +x "$name" && echo "$bash_path" >> "$name" && printf "${green}Created $name" || error
 
-	mousepad "$name" &  # open file in subshell aka background
+        [ $is_py_script == true ] && add_ext 'py' || add_ext 'sh'
+
+
+        [ -e "$name" ] && printf "\n${red}$name already exists" && error    # check if file with the same name exits
+
+        # create file with perms and interpeter path
+
+
+        [ "$is_py_script" == true ] && create_script "$python_path" || create_script "$bash_path"
+
+        vim "$name"     # open file script in vim
 }
 
 
